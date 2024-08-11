53914fa304ef1f75c57fd87be99c5bf8ecaef743
tolu <103815106+teuniced@users.noreply.github.com>
2024-08-11 14:56:08 -0400

    Team 8 modified the solution class for challenge 1(the average sum of a salary excluding min and max salaries), to include comments

--- src/main/java/com/bptn/leetcode_challenge_team_8/leetcode/challenge1/Main.java
+++ src/main/java/com/bptn/leetcode_challenge_team_8/leetcode/challenge1/Main.java
@@ -3,24 +3,25 @@ package com.bptn.leetcode_challenge_team_8.leetcode.challenge1;
 public class Main {
 
 	public static void main(String[] args) {
-	       Solution solution = new Solution();
-	        //Test case 1
-	        int[] salary1 = {1000,2000,3000};
-	        System.out.println("Test case 1: " + solution.average(salary1));
+		Solution solution = new Solution();
+		// Test case 1: Testing the method with an array of integers
+		int[] salary1 = { 1000, 2000, 3000 };
+		System.out.println("Test case 1: " + solution.average(salary1));
 
-	        //Test case 2
-	        int[] salary2 = {8000,9000,2000,3000,6000,1000};
-	        System.out.println("Test case 2: " + solution.average(salary2));
+		// Test case 2:Testing the method with an array of integers
+		int[] salary2 = { 8000, 9000, 2000, 3000, 6000, 1000 };
+		System.out.println("Test case 2: " + solution.average(salary2));
 
-	        //Test case 3
-	        int[] salary3 = {4000,3000,1000,2000};
-	        System.out.println("Test case 3: " + solution.average(salary3));
-	        
-	        //This is to test the time complexity
-	        long startTime = System.currentTimeMillis();
-	        long endTime = System.currentTimeMillis();
-	        long duration = (endTime - startTime);
-	        System.out.println("Execution time: " + duration + " milliseconds");
+		// Test case 3: Testing the method with an array of integers
+		int[] salary3 = { 4000, 3000, 1000, 2000 };
+		System.out.println("Test case 3: " + solution.average(salary3));
+
+		// The execution time of the method - This is to check for the time complexity
+		// of the method
+		long startTime = System.currentTimeMillis();
+		long endTime = System.currentTimeMillis();
+		long duration = (endTime - startTime);
+		System.out.println("Execution time: " + duration + " milliseconds");
 
 	}
 
--- src/main/java/com/bptn/leetcode_challenge_team_8/leetcode/challenge1/Solution.java
+++ src/main/java/com/bptn/leetcode_challenge_team_8/leetcode/challenge1/Solution.java
@@ -1,22 +1,35 @@
 package com.bptn.leetcode_challenge_team_8.leetcode.challenge1;
 
+//This class contains the solution to solve the challenge
 public class Solution {
+	// This method calculates the average salaries excluding the min and max
+	// salaries.
 	public double average(int[] salary) {
+
+		// Initializing the variables and the final calculated average
+		// and the return type as stated from leetcode.
 		double result, sum = 0;
+
+		// Initializing the variables with the first element of the
+		// salary array before it begins its iteration across the loop
 		int min = salary[0];
 		int max = salary[0];
 
+		// Calculating the length for average which excludes min and max
 		int length = (salary.length) - 2;
 
+		// iterating through all salaries using a for each loop
 		for (int salaries : salary) {
 			sum += salaries;
+			// Conditional statement to update the min salary if a lower/higher salary
+			// is found when looping across the index
 			if (salaries < min)
 				min = salaries;
 			if (salaries > max)
 				max = salaries;
 		}
-		
-		result =(sum-min-max)/length;
+		// Calculating the average salary excluding the min and max salaries.
+		result = (sum - min - max) / length;
 		return result;
 	}
 }
